2cc2bff929730b47b2a47aae4e34f8779c3018fc
DxniChid <chidiroglou.danai06@gmail.com>
2025-06-23 01:06:28 +0200

    Sun created

--- main.js
+++ main.js
@@ -6,3 +6,9 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+
+const sunGeometry = new THREE.SphereGeometry(5, 30, 30);
+const sunMaterial = new THREE.MeshBasicMaterial({color: 0xffff00 });
+const sun = new THREE.Mesh(sunGeometry, sunMaterial);
+scene.add(sun);
