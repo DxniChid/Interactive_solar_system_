9756b5d827976b2d56ef2a8668821f0e1d03e275
DxniChid <chidiroglou.danai06@gmail.com>
2025-06-23 21:26:53 +0200

    Mars added
    - I also added positions of the other planets so theyre visible

--- solar.js
+++ solar.js
@@ -8,10 +8,19 @@ const earthGeometry = new THREE.SphereGeometry(2, 32, 32);
 const earthMaterial = new THREE.MeshBasicMaterial({color:0x0000ff});
 const earth = new THREE.Mesh(earthGeometry, earthMaterial);
 scene.add(earth);
+earth.position.x = 15;
 
 const moonGeometry = new THREE.SphereGeometry(0.5, 32, 32);
 const moonMaterial = new THREE.MeshBasicMaterial({color: 0x888888});
 const moon = new THREE.Mesh(moonGeometry, moonMaterial);
+scene.add(moon);
+moon.position.x = 18;
+
+const marsGeometry = new THREE.SphereGeometry(1.5, 32, 32);
+const marsMaterial = new THREE.MeshBasicMaterial({color: 0x0000ff });
+const mars = new THREE.Mesh(marsGeometry, marsMaterial);
+scene.add(mars);
+mars.position.x = 22;
 
 window.addEventListener('resize', ()=> {
     camera.aspect = window.innerWidth / window.innerHeight;
